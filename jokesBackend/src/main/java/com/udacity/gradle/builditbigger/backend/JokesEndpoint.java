444f2c5a69403d29e819be8b39699aed7bab83fa
James Shvarts <jshvarts@gmail.com>
2016-03-20 02:51:32 -0400

    added backend

---
+++ jokesBackend/src/main/java/com/udacity/gradle/builditbigger/backend/JokesEndpoint.java
@@ -0,0 +1,36 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.udacity.gradle.builditbigger.jokes.Joke;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+  name = "myApi",
+  version = "v1",
+  namespace = @ApiNamespace(
+    ownerDomain = "backend.builditbigger.gradle.udacity.com",
+    ownerName = "backend.builditbigger.gradle.udacity.com",
+    packagePath=""
+  )
+)
+public class JokesEndpoint {
+
+    /** A simple endpoint method that retrieves and returns a joke */
+    @ApiMethod(name = "tellJoke")
+    public JokeBean tellJoke() {
+        JokeBean response = new JokeBean();
+        response.setData(new Joke().getJoke());
+        return response;
+    }
+
+}
