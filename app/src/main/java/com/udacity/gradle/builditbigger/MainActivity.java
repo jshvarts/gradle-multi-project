3ceb7f276cc3f6d68d379f498f1af428374bc4d0
James Shvarts <jshvarts@gmail.com>
2016-03-20 04:41:57 -0400

    added local backend

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,25 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 import com.udacity.gradle.builditbigger.jokeintenthandler.HandleJokeActivity;
 import com.udacity.gradle.builditbigger.jokes.Joke;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -42,8 +54,53 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, HandleJokeActivity.class);
-        intent.putExtra(HandleJokeActivity.JOKE_TEXT_EXTRA, new Joke().getJoke());
-        startActivity(intent);
+        new EndpointsAsyncTask(this).execute();
+    }
+
+    private class EndpointsAsyncTask extends AsyncTask<Object, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        public EndpointsAsyncTask(Context context) {
+            if (context == null) {
+                Log.e(MainActivity.class.getSimpleName(), "null context passed in");
+            }
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Object... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setApplicationName(getString(R.string.app_name))
+                        // options for running against local devappserver
+                        // - 10.0.3.2 is localhost's IP address in Genymotion
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.3.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(context, HandleJokeActivity.class);
+            intent.putExtra(HandleJokeActivity.JOKE_TEXT_EXTRA, result);
+            startActivity(intent);
+        }
     }
 }
